c472ba3205a4334c4f7a61197953082657f0c0a0
Timur079 <144673079+Timur079@users.noreply.github.com>
2024-04-17 14:57:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,30 @@ def func01():
     return func_sort(lst)
 
 print(func01())
+
+fun bubbleSort(numbers: MutableList<Int>) {
+    val n = numbers.size
+    for (i in 0 until n - 1) {
+        for (j in 0 until n - i - 1) {
+            if (numbers[j] > numbers[j + 1]) {
+                // Обмен значениями
+                val temp = numbers[j]
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp
+            }
+        }
+    }
+}
+
+fun main() {
+    println("Введите числа, разделенные пробелом:")
+    val input = readLine()
+    val numbers = input?.split(" ")?.map { it.toInt() }?.toMutableList()
+
+    if (numbers != null) {
+        bubbleSort(numbers)
+        println("Отсортированный список: $numbers")
+    } else {
+        println("Ошибка: введен некорректный список чисел.")
+    }
+}

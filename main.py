f29e22352f81f40fd67052edadf70c006a6e25d7
Timur079 <144673079+Timur079@users.noreply.github.com>
2024-05-03 11:16:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def func_sort(lst):
-    return sorted(lst)
-
-def func01():
-    lst = [9, 1, 7, 3, 5, 4, 6, 2, 8]
-    return func_sort(lst)
-
-print(func01())
-
 fun bubbleSort(numbers: MutableList<Int>) {
     val n = numbers.size
     for (i in 0 until n - 1) {
